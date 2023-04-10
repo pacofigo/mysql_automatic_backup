d65c2415edbef5204e4e57a46e213614bdf3dc99
Francisco Javier Ferreyra López <pacofigo@outlook.com>
2023-04-10 09:16:22 -0600

    Create local_backup.sh

---
+++ local_backup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#Local path
+local_path="/local/path"
+
+#Get system date
+date=$(date +"%d-%b-%Y")
+
+#Generate the backup
+mysqldump database_name > $local_path/database_nameBackup_$date.sql
+
+#Compress the backup
+tar -czvf $local_path/database_nameBackup_$date.sql.tar.gz $local_path/database_nameBackup_$date.sql
+
+#Delete the SQL file
+rm $local_path/database_nameBackup_$date.sql
